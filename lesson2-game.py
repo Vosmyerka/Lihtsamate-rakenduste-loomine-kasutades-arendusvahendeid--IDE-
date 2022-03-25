d23480556b28717b3c9b5ecf335533cb2e2dedf6
Vosmyerka <npolevtsov@gmail.com>
2022-03-25 09:50:54 +0200

    Update lesson2-game.py
    can move left now

--- lesson2-game.py
+++ lesson2-game.py
@@ -10,7 +10,13 @@ map2 = [
     [1, 0, 0, 0],
     [0, 0, 0, 24]
 ]
-map = map2
+map3 = [
+    [12, 1, 0, 0],
+    [0, 0, 1, 0],
+    [1, 0, 1, 0],
+    [24, 0, 0, 0]
+]
+map = map3
 
 # define starting positions
 start_position_x = 0
@@ -24,17 +30,25 @@ def move(step, current_position_x, current_position_y):
 
     can_move_right = current_position_x <= 2 and map[current_position_y][current_position_x+1] == 0
     can_move_bottom = current_position_y <= 2 and map[current_position_y+1][current_position_x] == 0
+    can_move_left = current_position_x <= 2 and map[current_position_y][current_position_x-1] == 0
+    
 
     right_is_finish = current_position_x <= 2 and map[current_position_y][current_position_x+1] == 24
     bottom_is_finish = current_position_y <= 2 and map[current_position_y+1][current_position_x] == 24
+    left_is_finish = current_position_x <= 2 and map[current_position_y][current_position_x-1] == 24
+    
 
     if right_is_finish:
         print("Found finish on right")
         return False
-
     if bottom_is_finish:
         print("Found finish on bottom")
         return False
+    if left_is_finish:
+        print("Found finish on left")
+        return False
+
+
 
     if can_move_right:
         print("Should move right")
@@ -42,6 +56,10 @@ def move(step, current_position_x, current_position_y):
     if can_move_bottom:
         print("Should move down")
         return[current_position_x, current_position_y + 1]
+    if can_move_left:
+        print("Should move left")
+        return[current_position_x - 1, current_position_y]
+
 
 current_move_number = 1
 new_position = move(current_move_number, start_position_x, start_position_y)
